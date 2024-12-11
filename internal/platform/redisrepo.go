0bdc86592d5e63132401eda7fc95515fc3c087af
sibuh <misgandess@gmail.com>
2024-12-11 19:23:50 +0300

    removed former files from projects

--- internal/platform/redisrepo.go
+++
@@ -1,8 +0,0 @@
-package platform
-
-import "context"
-
-type RedisInterface interface {
-	SetToRedis(ctx context.Context, key string, value interface{}) error
-	GetFromRedis(ctx context.Context, key string) (string, error)
-}
