31c287468a358536b18a6538f54972788dd5c5d3
Misganaw <misgandess@gmail.com>
2025-01-24 17:01:21 +0300

    updated create room type

--- internal/service/room_type/model.go
+++ internal/service/room_type/model.go
@@ -11,9 +11,9 @@ type CreateRoomTypeRequest struct {
 
 func (c CreateRoomTypeRequest) Validate() error {
 	return validation.ValidateStruct(&c,
-		validation.Field(c.RoomType, validation.Required.Error("room_type is required")),
-		validation.Field(c.Description, validation.Required.Error("description is required")),
-		validation.Field(c.Price, validation.Required.Error("price is required")),
-		validation.Field(c.Capacity, validation.Required.Error("capacity is required")),
+		validation.Field(&c.RoomType, validation.Required.Error("room_type is required")),
+		validation.Field(&c.Description, validation.Required.Error("description is required")),
+		validation.Field(&c.Price, validation.Required.Error("price is required")),
+		validation.Field(&c.Capacity, validation.Required.Error("capacity is required")),
 	)
 }
